87841022f4bcf9910a8430327a147d78b28481b7
Neha Vishwakarma <71029390+Neha649@users.noreply.github.com>
2022-01-18 14:30:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 def Home():
     return render_template('index.html')
 
-
+#this is a comment hellooooooooooooo
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
